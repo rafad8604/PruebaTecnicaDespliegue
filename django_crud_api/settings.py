7cc0aa75db5add748ecec6fab9928fd66f95ff94
brian <brduran8@gmail.com>
2025-10-30 09:32:46 -0500

    Prueba técnica finalizada. Terminada en local, lista para despliegue.

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -145,7 +145,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://prueba-tecnica-rafad8604-brian-durans-projects.vercel.app/",
+    "https://prueba-tecnica-rafad8604-brian-durans-projects.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
